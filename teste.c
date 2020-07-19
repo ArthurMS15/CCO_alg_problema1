39d227e9249ed6b9fe3f7c4b0686132b6999408d
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2020-07-19 17:17:39 -0300

    Alteração
    Adicionado outros tipos de filtros para mudança da matriz

--- teste.c
+++ teste.c
@@ -69,6 +69,79 @@ int main(){
             printf("\n");
             }
             break;
+        case 3:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
+                        printf("\t");
+                    }
+                    else{
+                        matrizaux[l][c]=((matriz[l-1][c-1]*-1)+(matriz[l-1][c]*-1)+(matriz[l-1][c+1]*-1)+(matriz[l][c-1]*-1)+(matriz[l][c]*8)+(matriz[l][c+1]*-1)+(matriz[l+1][c-1]*-1)+(matriz[l+1][c]*-1)+(matriz[l+1][c+1]*-1));
+                        printf("%i\t", matrizaux[l][c]);
+                    }
+                }
+            printf("\n");
+            }
+            break;
+        case 4:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
+                        printf("\t");
+                    }
+                    else{
+                        matrizaux[l][c]=((matriz[l-1][c-1]*-2)+(matriz[l-1][c]*-1)+(matriz[l-1][c+1]*0)+(matriz[l][c-1]*-1)+(matriz[l][c]*1)+(matriz[l][c+1]*1)+(matriz[l+1][c-1]*0)+(matriz[l+1][c]*1)+(matriz[l+1][c+1]*2));
+                        printf("%i\t", matrizaux[l][c]);
+                    }
+                }
+            printf("\n");
+            }
+            break;
+        case 5:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
+                        printf("\t");
+                    }
+                    else{
+                        matrizaux[l][c]=(matriz[l-1][c-1]*-1)+(matriz[l-1][c]*0)+(matriz[l-1][c+1]*0)+(matriz[l][c-1]*1)+(matriz[l][c]*0)+(matriz[l][c+1]*0)+(matriz[l+1][c-1]*0)+(matriz[l+1][c]*0)+(matriz[l+1][c+1]*0);
+                        printf("%i\t", matrizaux[l][c]);
+                    }
+                }
+            printf("\n");
+            }
+            break;
+        case 6:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
+                        printf("\t");
+                    }
+                    else{
+                        matrizaux[l][c]=(matriz[l-1][c-1]*-1)+(matriz[l-1][c]*0)+(matriz[l-1][c+1]*1)+(matriz[l][c-1]*-2)+(matriz[l][c]*0)+(matriz[l][c+1]*2)+(matriz[l+1][c-1]*-1)+(matriz[l+1][c]*0)+(matriz[l+1][c+1]*1);
+                        printf("%i\t", matrizaux[l][c]);
+                    }
+                }
+            printf("\n");
+            }
+            break;
+        case 7:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
+                        printf("\t");
+                    }
+                    else{
+                        matrizaux[l][c]=(matriz[l-2][c-2]*0)+(matriz[l-2][c-1]*0)+(matriz[l-2][c]*0)+(matriz[l-2][c+1]*0)+(matriz[l-2][c+2]*0)+(matriz[l-1][c-2]*0)+(matriz[l-1][c-1]*1)+(matriz[l-1][c]*1)+(matriz[l-1][c+1]*1)+(matriz[l-1][c+2]*0)+(matriz[l][c-2]*0)+(matriz[l][c+1]*1)+(matriz[l][c]*1)+(matriz[l][c+1]*1)+(matriz[l][c+2]*0)+(matriz[l+1][c-2]*0)+(matriz[l+1][c-1]*1)+(matriz[l+1][c]*1)+(matriz[l+1][c+1]*1)+(matriz[l+1][c+2]*0)+(matriz[l+2][c-2]*0)+(matriz[l+2][c-1]*0)+(matriz[l+2][c]*0)+(matriz[l+2][c+1]*0)+(matriz[l+2][c+2]*0);
+                        printf("%i\t", matrizaux[l][c]);
+                    }
+                }
+            printf("\n");
+            }
+            break;
+        case 8:
+            return 0;
+            break;
     }
     return 0;
 }
