6931ff8f150c905052edc09e521d49d7186f1869
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2020-07-19 14:25:51 -0300

    Alteração
    Mais opções de filtros e teste da função

--- teste.c
+++ teste.c
@@ -30,10 +30,14 @@ int main(){
 
     int var;
     printf("\nMatrizes para a convolucao:\n");
-    printf("1 - realcar bordas:\n");
-    printf("2 - detectar bordas:\n");
-    printf("3 - outra forma de detectar bordas:\n");
-    printf("4 - destacar relevo:\n");
+    printf("1 - realcar bordas (3x3):\n");
+    printf("2 - detectar bordas (3x3):\n");
+    printf("3 - outra forma de detectar bordas (3x3):\n");
+    printf("4 - destacar relevo (3x3):\n");
+    printf("5 - destacar objetos (3x3):\n");
+    printf("6 - exemplo do trabalho (3x3):\n");
+    printf("7 - desfocar a imagem (5x6):\n");
+    printf("0 - para finalizar o programa\n");
     scanf("%i", &var);
     switch(var){
         case 1:
@@ -41,6 +45,9 @@ int main(){
                 for(int c=0;c<colunamax;c++){
                     if(l==0 || c==0){
                         matriz[l][c]=0;
+                    }
+                    else{
+                        matriz[l][c]=((matriz[l-1][c-1]*0)+(matriz[l-1][c]*0)+(matriz[l-1][c+1]*0)+(matriz[l][c-1]*-1)+(matriz[l][c]*1)+(matriz[l][c+1]*0)+(matriz[l+1][c-1]*0)+(matriz[l+1][c]*0)+(matriz[l+1][c+1]*0));
                     }
                     printf("%i\t", matriz[l][c]);
                 }
