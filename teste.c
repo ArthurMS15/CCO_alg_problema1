297075344117dabda8fa13a5b017cbcdab830ee5
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2020-07-18 21:07:16 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(){
+
     int colunamax;
     printf("(n inteiro) Insira a dimensao x (coluna) da matriz que passara pela convolucao:\n");
     scanf("%i", &colunamax);
@@ -20,6 +19,8 @@ int main(){
             scanf("%i", &matriz[l][c]);
         }
     }
+
+    printf("\n");
     for(int l=0;l<linhamax;l++){
         for(int c=0;c<colunamax;c++){
             printf("%i\t", matriz[l][c]);
@@ -27,7 +28,25 @@ int main(){
         printf("\n");
     }
 
+    int var;
     printf("\nMatrizes para a convolucao:\n");
-    printf("1 - realçar bordas:\n");
-    
+    printf("1 - realcar bordas:\n");
+    printf("2 - detectar bordas:\n");
+    printf("3 - outra forma de detectar bordas:\n");
+    printf("4 - destacar relevo:\n");
+    scanf("%i", &var);
+    switch(var){
+        case 1:
+            for(int l=0;l<linhamax;l++){
+                for(int c=0;c<colunamax;c++){
+                    if(l==0 || c==0){
+                        matriz[l][c]=0;
+                    }
+                    printf("%i\t", matriz[l][c]);
+                }
+            printf("\n");
+            }
+            break;
+    }
+    return 0;
 }
