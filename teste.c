2e7fdea397c95aafc84febc7990b9df3b59ea030
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2020-08-04 15:23:12 -0300

    Alteração
    Tirado um dos casos para simplificação do código

--- teste.c
+++ teste.c
@@ -22,7 +22,6 @@ void KernelCaso3(int linhamax, int colunamax, int **matriz, int **matrizaux);
 void KernelCaso4(int linhamax, int colunamax, int **matriz, int **matrizaux);
 void KernelCaso5(int linhamax, int colunamax, int **matriz, int **matrizaux);
 void KernelCaso6(int linhamax, int colunamax, int **matriz, int **matrizaux);
-void KernelCaso7(int linhamax, int colunamax, int **matriz, int **matrizaux);
 void FinalizarPrograma(); // *modularização(procedimento e de ação), prototipação
 
 int main(){
@@ -89,7 +88,6 @@ int EscolhaKernel(){
     printf("4 - destacar relevo (3x3):\n");
     printf("5 - destacar objetos (3x3):\n");
     printf("6 - exemplo do trabalho (3x3):\n");
-    printf("7 - desfocar a imagem (5x6):\n");
     printf("0 - para finalizar o programa\n");
     scanf("%i", &var);
     return var;
@@ -132,9 +130,6 @@ void ProcessoConvolucaoeEscolhaKernel(int linhamax, int colunamax, int **matriz,
             break;
         case 6:
             KernelCaso6(linhamax, colunamax, matriz, matrizaux);
-            break;
-        case 7:
-            KernelCaso7(linhamax, colunamax, matriz, matrizaux);
             break;
         case 0:
             FinalizarPrograma();
@@ -225,21 +220,6 @@ void KernelCaso6(int linhamax, int colunamax, int **matriz, int **matrizaux){
             }
             else{
                 matrizaux[l][c]=(matriz[l-1][c-1]*-1)+(matriz[l-1][c]*0)+(matriz[l-1][c+1]*1)+(matriz[l][c-1]*-2)+(matriz[l][c]*0)+(matriz[l][c+1]*2)+(matriz[l+1][c-1]*-1)+(matriz[l+1][c]*0)+(matriz[l+1][c+1]*1);
-                printf("%i\t", matrizaux[l][c]);
-            }
-        }
-        printf("\n");
-    }
-}
-
-void KernelCaso7(int linhamax, int colunamax, int **matriz, int **matrizaux){
-    for(int l=0;l<linhamax;l++){
-        for(int c=0;c<colunamax;c++){
-            if(l==0 || c==0 || l==linhamax-1 || c==colunamax-1){
-                printf("\t");
-            }
-            else{
-                matrizaux[l][c]=(matriz[l-2][c-2]*0)+(matriz[l-2][c-1]*0)+(matriz[l-2][c]*0)+(matriz[l-2][c+1]*0)+(matriz[l-2][c+2]*0)+(matriz[l-1][c-2]*0)+(matriz[l-1][c-1]*1)+(matriz[l-1][c]*1)+(matriz[l-1][c+1]*1)+(matriz[l-1][c+2]*0)+(matriz[l][c-2]*0)+(matriz[l][c+1]*1)+(matriz[l][c]*1)+(matriz[l][c+1]*1)+(matriz[l][c+2]*0)+(matriz[l+1][c-2]*0)+(matriz[l+1][c-1]*1)+(matriz[l+1][c]*1)+(matriz[l+1][c+1]*1)+(matriz[l+1][c+2]*0)+(matriz[l+2][c-2]*0)+(matriz[l+2][c-1]*0)+(matriz[l+2][c]*0)+(matriz[l+2][c+1]*0)+(matriz[l+2][c+2]*0);
                 printf("%i\t", matrizaux[l][c]);
             }
         }
