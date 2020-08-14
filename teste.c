4cbe834d3b6f2555b63f1989e12e28e6ad1ccefd
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2020-08-14 01:45:31 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -161,8 +161,8 @@ int convolucaoCaso1(int l, int c, int **matriz, int **kernel){
     int aux=c;
     l--; 
     c--;
-    for(int lk=0; lk < 3; lk++){
-        for(int ck = 0; ck < 3; ck++){
+    for(int lk=0;lk<3;lk++){
+        for(int ck=0;ck<3;ck++){
             matrizaux = matriz[l][c] * kernel[lk][ck];
             auxmatrizaux=auxmatrizaux+matrizaux;
             c++;
